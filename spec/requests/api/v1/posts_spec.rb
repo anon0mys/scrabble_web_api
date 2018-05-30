a9a35d469f44dad61dfe1e82c69f701aa9620b63
Evan Wheeler <anon0mys87@gmail.com>
2018-05-30 11:52:06 -0600

    Moves plays request testing into its own file

---
+++ spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'API::V1::Games::Plays' do
+  before(:all) do
+    DatabaseCleaner.clean
+    Rails.application.load_seed
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  context 'POST /api/v1/games/1/plays' do
+    it 'should allow a user to post a word and update their score' do
+      params = { user_id: 1, word: 'at' }
+
+      post '/api/v1/games/1/plays', params: params
+
+      expect(response.status).to eq(201)
+
+      get '/api/v1/games/1'
+
+      expected_json = { game_id: 1,
+                        scores: [
+                          { user_id: 1,
+                            score: 17 },
+                          { user_id: 2,
+                            score: 16 }
+                        ] }.to_json
+
+      expect(response.body).to eq(expected_json)
+    end
+
+    it 'does not score invalid words' do
+      word = 'foxez'
+      params = { user_id: 1, word: word }
+
+      post '/api/v1/games/1/plays', params: params
+
+      expected_json = {"message": "'#{word}' is not a valid word."}.to_json
+
+      expect(response.body).to eq(expected_json)
+    end
+  end
+end
