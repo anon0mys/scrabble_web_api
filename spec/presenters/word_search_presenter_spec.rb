e70e85da621b1b73fedbd0d1973c74a9df068875
Evan Wheeler <anon0mys87@gmail.com>
2018-05-30 11:49:24 -0600

    Adds invalid word testing to word search presenter

--- spec/presenters/word_search_presenter_spec.rb
+++ spec/presenters/word_search_presenter_spec.rb
@@ -11,12 +11,34 @@ describe WordSearchPresenter do
 
   context 'instance methods' do
     context '#result' do
-      it 'should validate a real word' do
+      it 'should create a response for a real word' do
         presenter = WordSearchPresenter.new('foxes')
 
         expected = "'foxes' is a valid word and its root form is 'fox'."
 
         expect(presenter.result).to eq(expected)
+      end
+
+      it 'should create a different response for a fake word' do
+        presenter = WordSearchPresenter.new('foxez')
+
+        expected = "'foxez' is not a valid word."
+
+        expect(presenter.result).to eq(expected)
+      end
+    end
+
+    context '#valid_word?' do
+      it 'should validate a real word' do
+        presenter = WordSearchPresenter.new('foxes')
+
+        expect(presenter.valid_word?).to be true
+      end
+
+      it 'should not validate a fake word' do
+        presenter = WordSearchPresenter.new('foxez')
+
+        expect(presenter.valid_word?).to be false
       end
     end
   end
