f528e1ff9e09631895c09a107d7e0f64afd66076
Evan Wheeler <anon0mys87@gmail.com>
2018-06-03 16:03:59 -0600

    Refactoras service to allow make_request to take different endpoints, moves enpoint allocation into instance methods

--- app/services/word_search_service.rb
+++ app/services/word_search_service.rb
@@ -1,11 +1,13 @@
 class WordSearchService
   def initialize(word)
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1/inflections/en/#{word}")
+    @conn = Faraday.new(url: 'https://od-api.oxforddictionaries.com')
+    @word = word
   end
 
   def validate_word
     begin
-      response = make_request
+      endpoint = '/api/v1/inflections/en/' + @word
+      response = make_request(endpoint)
       JSON.parse(response.body, symbolize_names: true)
     rescue JSON::ParserError => e
       e.message
@@ -14,10 +16,11 @@ class WordSearchService
 
   private
 
-  def make_request
+  def make_request(endpoint)
     headers = {'app_id' => ENV['OXFORD_APP_ID'], 'app_key' => ENV['OXFORD_API_KEY']}
     @conn.get do |req|
       req.headers = headers
+      req.url endpoint
     end
   end
 end
